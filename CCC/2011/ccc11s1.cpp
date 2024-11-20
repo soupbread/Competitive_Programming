5b298cf12f82ac42f1b5fa328540fe5fba27e6ae
soupbread <96434847+soupbread@users.noreply.github.com>
2024-11-20 00:05:54 -0500

    Problem 2024/11/20

---
+++ CCC/2011/ccc11s1.cpp
@@ -0,0 +1,29 @@
+// AC 2024/11/20
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n, t_cnt=0, s_cnt=0;
+    string sentence;
+    cin >> n;
+    cin.ignore();
+    for(int i=0;i<n;i++){
+        getline(cin, sentence);
+        for(char ch : sentence){
+            if(ch=='t' || ch=='T'){
+                t_cnt++;
+            }
+            else if(ch=='s' || ch=='S'){
+                s_cnt++;
+            }
+        }
+    }
+    if(s_cnt>=t_cnt){
+        cout << "French" << endl;
+    }
+    else{
+        cout << "English" << endl;
+    }
+}
