88d7ff8cd5b03b29701b6232f45fbe7499e05ca3
soupbread <96434847+soupbread@users.noreply.github.com>
2024-11-25 00:00:21 -0500

    Problem 2024/11/25

---
+++ acc7p1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    for(int i=0;i<t;i++){
+        int n;
+        cin >> n;
+        if(n==2) cout << 2 << endl;
+        else cout << n-1 << endl;
+    }
+}
---
+++ dmopc22c1p1.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t, n;
+    bool can_make = true, start_up = true;
+    scanf("%i", &t);
+    // massively overcomplicating be like
+}
