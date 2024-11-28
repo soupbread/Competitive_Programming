7ec9a862f2729621f58acc4a98930f78309699af
soupbread <96434847+soupbread@users.noreply.github.com>
2024-11-27 23:23:46 -0500

    Problem 2024/11/27 in my heart (really 11/28)

---
+++ CCC/2004/ccc04j1.cpp
@@ -0,0 +1,10 @@
+// AC 2024/11/27 (actually, 11/28)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    cout << "The largest square has side length " << int(sqrt(n)) << "." << endl;
+}
