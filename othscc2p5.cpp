1e7259e7b049b288ff2af8a7f1d59d8b4d768c66
soupbread <96434847+soupbread@users.noreply.github.com>
2024-12-02 08:22:35 -0500

    test

--- othscc2p5.cpp
+++ othscc2p5.cpp
@@ -2,13 +2,8 @@
 using namespace std;
 
 int main(){
-    int cnt=0, n, m;
-    char school[n][m+1];
-    cin >> n >> m;
-    for(int i=0; i<n; i++){
-        string line;
-        cin >> line;
-        strcpy(school[i], line.c_str());
-    }
-    return 0;
+    int n, m;
+    scanf("%i %i", &n, &m);
+    
+    return 0;  
 }
