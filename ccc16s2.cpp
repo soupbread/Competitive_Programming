a13d48947ee655cbc596f4272fbb2cbd8a4c8b39
soupbread <96434847+soupbread@users.noreply.github.com>
2024-12-05 22:39:45 -0500

    Problem 12/5

--- ccc16s2.cpp
+++ ccc16s2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int q, n, ans;
+    int q, n, ans=0;
     cin >> q >> n;
     vector<int> d(n), p(n);
     for(int i=0; i<n; i++){
@@ -13,15 +13,15 @@ int main(){
     }
     if(q==1){
         sort(d.begin(), d.end());
-        sort(p.begin(), d.end());
+        sort(p.begin(), p.end());
         for(int i=0; i<n; i++){
             ans+=max(d[i],p[i]);
         }
     }
     else{
         sort(d.begin(), d.end());
-        sort(p.end(), d.begin());
-        for(int i=n-1; i>=0; i++){
+        sort(p.begin(), p.end(), greater<>());
+        for(int i=0; i<n; i++){
             ans+=max(d[i],p[i]);
         }
     }
