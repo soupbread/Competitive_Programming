0b74f61989b821d5ed7c28afbc78f79b68d33222
soupbread <96434847+soupbread@users.noreply.github.com>
2024-12-08 00:03:39 -0500

    Attempt 2024/12/08

--- dmopc15c6p3.cpp
+++ dmopc15c6p3.cpp
@@ -1,65 +1,34 @@
 // Initial attempt 2024/11/29
+// Second attempt 2024/12/08
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-    int n, m, k, min_len = INT_MAX;
+    int n, m, k, min_len = INT_MAX, r=n-1;
     bool can = false;
     scanf("%i %i %i", &n, &m, &k); // n = rows, m = cols
-    vector<vector<int>> field(m+1, vector<int>(n, 0));
-    vector<int> sums(n);
-
-    for(int col=0; col<m; col++){
+    vector<int> rows(n, m), sums(n+1, 0);
+    for(int i=0; i<m; i++){
         int a, b;
-        cin >> a >> b;
-        field[col][a-1]=1;
-        field[col][b]=1;
-    }
-
-    for(int col=0; col<m; col++){
-        for(int row=0; row<n; row++){
-            
-            cout << field[row][col] << " ";
+        scanf("%i%i", &a, &b);
+        for(int j=a-1; j<b; j++){
+            rows[j]--;
         }
-        cout << endl;
     }
 
+    for(int i=1; i<=n; i++){
+        sums[i]=sums[i-1]+rows[i-1];
+    }
 
-
-    // attempt 1: brute force
-    // for(int i=0; i<m; i++){
-    //     int a, b;
-    //     scanf("%i %i", &a, &b);
-    //     for(int j=a-1; j<b; j++){
-    //         field[j][i] = 0;
-    //     }
-    // }
-
-    // for(int i=1; i<n; i++){
-    //     for(int j=1; j<=m; j++){
-    //         sums[i][j] = sums[i][j-1]+field[i][j-1];
-    //     }
-    // }
-
-    // // for(int i=0; i<=n; i++){
-    // //     for(int j=0; j<=m; j++){
-    // //         cout << sums[i][j] << " ";
-    // //     }
-    // //     cout << "\n";
-    // // }
-
-    // for(int left=1; left<=m; left++){
-    //     int c_sum = 0;
-    //     for(int right=left; right<n; right++){
-    //         c_sum+=sums[right][m];
-    //         if(c_sum>=k){
-    //             min_len = min(right-left+1, min_len);
-    //             can = true;
-    //             break;
-    //         }
-    //     }
-    // }
-    // if(can) cout << min_len << endl;
-    // else cout << -1 << endl;
+    for(int i=0; i<n; i++){
+        for(int j=n-1; j>=i; j--){
+            if(sums[j+1]-sums[i]>=k && j-i+1<min_len){
+                can=true;
+                min_len=j-i+1;
+            }
+        }
+    }
+    if(can) cout << min_len << endl;
+    else cout << -1 << endl;
     return 0;
 }
