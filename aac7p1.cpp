4dfd48f5d9d2b0f58c8f197eb71d6582882ebacd
soupbread <96434847+soupbread@users.noreply.github.com>
2024-12-20 00:27:18 -0500

    p20241219

---
+++ aac7p1.cpp
@@ -0,0 +1,15 @@
+// AC 2024/12/20
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    for(int i=0; i<t; i++){
+        int w, h;
+        cin >> w >> h;
+        if(w<4 && h<4) cout << "bad" << endl;
+        else if(w==1 || h==1 && w<7) cout << "bad" << endl;
+        else cout << "good" << endl;
+    }
+}
