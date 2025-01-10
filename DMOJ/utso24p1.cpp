7947b0e0ac45bee4072d3687a42bf402f178e55a
soupbread <96434847+soupbread@users.noreply.github.com>
2025-01-09 19:28:29 -0500

    p20250109

--- DMOJ/utso24p1.cpp
+++ DMOJ/utso24p1.cpp
@@ -1,20 +1,13 @@
 // First attempt 2025/01/05
+// AC 2025/01/09
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(){
     long long n, k, cnt=0;
     cin >> n >> k;
-    if(k>=n){
-        cnt=n*2;
-    }
-    else{
-        for(int c=1; c<=int(n/k); c++){
-            cnt+=k*c*2;
-        }
-        if(n%k!=0){
-            cnt+=n*2;
-        }
+    for(int i=n; i>0; i-=k){
+        cnt+=i*2;
     }
     cout << cnt << endl;
     return 0;
