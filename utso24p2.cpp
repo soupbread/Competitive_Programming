ea92835721f54106e4bac77a09f80aaec5b7d415
soupbread <96434847+soupbread@users.noreply.github.com>
2025-01-10 23:48:32 -0500

    p20250110

--- utso24p2.cpp
+++ utso24p2.cpp
@@ -1,52 +1,54 @@
+// First attempt 2025/01/05
+// AC 2025/01/10
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
 int main(){
-    int n, k, pos_ind = 0, neg_ind = 0, ans=0;
-    vector<int> pos_gifts, neg_gifts;
+    ll n, k, pos_ind = 0, neg_ind = 0, ans=0;
+    vector<ll> pos_gifts, neg_gifts;
     cin >> n >> k;
-    int moves = k;
+    ll moves = k;
 
-    for(int i=0; i<n; i++){
-        int h;
+    for(long long i=0; i<n; i++){
+        ll h;
         cin >> h;
-        if(h>0) pos_gifts.push_back(h);
-        else if(h<0) neg_gifts.push_back(h);
+        if(h > 0) pos_gifts.push_back(h);
+        else if(h < 0) neg_gifts.push_back(h);
     }
 
-    sort(pos_gifts.begin(), pos_gifts.end(), greater<>());
+    sort(pos_gifts.begin(), pos_gifts.end(), greater<ll>());
     sort(neg_gifts.begin(), neg_gifts.end());
 
-    while(moves>0){
-        int pos_1 = 0, pos_2 = 0, neg = 0;
+    if(moves % 2 == 1){
+        if(pos_gifts.size() > 0 && pos_ind < ll(pos_gifts.size())){
+            ans+=pos_gifts[pos_ind];
+            pos_ind++;
+        }
+        moves--;
+    }
 
-        if(moves==1){
-            if(pos_gifts.size()>0 && pos_ind<int(pos_gifts.size())){
-                ans+=pos_gifts[pos_ind];
-            }
-            break;
+    while(moves > 0){
+        ll pos_1 = 0, pos_2 = 0, neg = 0;
+        
+        if(pos_gifts.size() > 0 && pos_ind < ll(pos_gifts.size())-1){
+            pos_1 = pos_gifts[pos_ind], pos_2 =  pos_gifts[pos_ind + 1];
+        }
+        else if(pos_gifts.size() > 0 && pos_ind <= ll(pos_gifts.size())-1){
+            pos_1 = pos_gifts[pos_ind];
+        }
+        if(neg_gifts.size() > 0 && neg_ind < ll(neg_gifts.size())){
+            neg = abs(neg_gifts[neg_ind]);
+        }
+        if(pos_1 + pos_2 > neg){
+            ans += pos_1 + pos_2;
+            pos_ind += 2;
         }
         else{
-            if(pos_gifts.size()>0 && pos_ind<int(pos_gifts.size())-1){
-                pos_1 = pos_gifts[pos_ind], pos_2 =  pos_gifts[pos_ind+1];
-            }
-            else if(pos_gifts.size()>0 && pos_ind<=int(pos_gifts.size())-1){
-                pos_1 = pos_gifts[pos_ind];
-            }
-            if(neg_gifts.size()>0 && neg_ind<int(neg_gifts.size())){
-                neg = abs(neg_gifts[neg_ind]);
-            }
-            if(pos_1 + pos_2 > neg){
-                moves--;
-                ans+=pos_1;
-                pos_ind++;
-            }
-            else{
-                moves-=2;
-                ans+=neg;
-                neg_ind++;
-            }
+            ans += neg;
+            neg_ind++;
         }
+        moves -= 2;
     }
     cout << ans << endl;
     return 0;
