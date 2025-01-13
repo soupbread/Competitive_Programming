654d33ed4c40316392f196e049bdd773bc691ce5
soupbread <96434847+soupbread@users.noreply.github.com>
2025-01-12 21:46:18 -0500

    p20250112

---
+++ DMOJ/ddrp1.cpp
@@ -0,0 +1,11 @@
+// AC 2025/01/12
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, m, p;
+    cin >> n >> m >> p;
+    if(n==m) cout << "Switch" << endl;
+    else cout << "Stay" << endl;
+    return 0;
+}
