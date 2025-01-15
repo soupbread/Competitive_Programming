f86e9896f51c803292c01c393f1d97461125258a
soupbread <96434847+soupbread@users.noreply.github.com>
2025-01-14 20:50:26 -0500

    p20250114

---
+++ DMOJ/bsspc21j1.cpp
@@ -0,0 +1,10 @@
+// AC 2025/01/14
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int s, x;
+    cin >> s >> x;
+    printf((s+2)*3+16<=x? "Yes it fits!" : "No, it's too small :(");
+    return 0;
+}
