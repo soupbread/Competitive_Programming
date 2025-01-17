d67d0a29232d8155e39a21c2d799280ae96a264b
soupbread <96434847+soupbread@users.noreply.github.com>
2025-01-16 21:26:23 -0500

    20250116

---
+++ DMOJ/ampl2024sp1.cpp
@@ -0,0 +1,13 @@
+// AC 2025/01/16
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string d;
+    cin >> d;
+    if(d != "Saturday" && d != "Sunday"){
+        cout << "YES" << endl;
+    }
+    else cout << "NO" << endl;
+    return 0;
+}
---
+++ usaco24decb1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t, cnt = 0;
+    cin >> t;
+    for(int i=0; i<t; i++){
+        int n, lower = 45, upper = 49;
+        cnt = 0;
+        cin >> n;
+
+        while(n >= lower){
+            if(n < upper){
+                cnt += n - lower + 1;
+            }
+            else cnt += upper - lower + 1;
+            lower = 4 * pow(10, to_string(lower).length()) + lower;
+            upper = upper * 10 + 9;
+        }
+        cout << cnt << endl;
+    }
+    return 0;
+}
