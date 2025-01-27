fb8de59d9114fc9a3eb7b738ad27114e60a638aa
soupbread <96434847+soupbread@users.noreply.github.com>
2025-01-26 22:10:03 -0500

    250126

--- CCC/CCCHK/Hkccc08j1.java
+++ CCC/CCCHK/Hkccc08j1.java
@@ -1,42 +1,42 @@
-import java.io.*;
-import java.util.*;
+// import java.io.*;
+// import java.util.*;
 
-public class Hkccc08j1 {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer in;
+// public class Hkccc08j1 {
+//     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//     static StringTokenizer in;
 
-    public static void main(String[] args) throws IOException {
-        int natalie=0, casper=0, temp, cNum=readInt();
+//     public static void main(String[] args) throws IOException {
+//         int natalie=0, casper=0, temp, cNum=readInt();
 
-        for(int i=0;i<cNum;i++){
-            temp=casper;
-            casper=readInt()*readInt();
-            if(temp>casper){
-                casper=temp;
-            }
-        }
+//         for(int i=0;i<cNum;i++){
+//             temp=casper;
+//             casper=readInt()*readInt();
+//             if(temp>casper){
+//                 casper=temp;
+//             }
+//         }
 
-        int nNum=readInt();
+//         int nNum=readInt();
 
-        for(int i=0;i<nNum;i++){
-            temp=natalie;
-            natalie=readInt()*readInt();
-            if(temp>natalie){
-                natalie=temp;
-            }
-        }
+//         for(int i=0;i<nNum;i++){
+//             temp=natalie;
+//             natalie=readInt()*readInt();
+//             if(temp>natalie){
+//                 natalie=temp;
+//             }
+//         }
 
-        if(casper>natalie) System.out.println("Casper");
-        else if(natalie>casper) System.out.println("Natalie");
-        else System.out.println("Tie");
-    }
+//         if(casper>natalie) System.out.println("Casper");
+//         else if(natalie>casper) System.out.println("Natalie");
+//         else System.out.println("Tie");
+//     }
 
-    static String next() throws IOException {
-        while (in == null || !in.hasMoreTokens())
-            in = new StringTokenizer(br.readLine());
-        return in.nextToken();
-    }
-    static int readInt() throws IOException {
-        return Integer.parseInt(next());
-    }
-}
+//     static String next() throws IOException {
+//         while (in == null || !in.hasMoreTokens())
+//             in = new StringTokenizer(br.readLine());
+//         return in.nextToken();
+//     }
+//     static int readInt() throws IOException {
+//         return Integer.parseInt(next());
+//     }
+// }
